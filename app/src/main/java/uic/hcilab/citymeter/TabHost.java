3e5cbd0ef7e8d84461caabaf9cde54a76b288bd7
yujaaa <yujaaa0219@gmail.com>
2018-06-05 11:45:44 -0500

    Add Bottom Navigation

---
+++ app/src/main/java/uic/hcilab/citymeter/TabHost.java
@@ -0,0 +1,49 @@
+package uic.hcilab.citymeter;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class TabHost extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_search:
+                    mTextMessage.setText(R.string.title_search);
+                    return true;
+                case R.id.navigation_activities:
+                    mTextMessage.setText(R.string.title_activities);
+                    return true;
+                case R.id.navigation_summary:
+                    mTextMessage.setText(R.string.title_summary);
+                    return true;
+            }
+            return false;
+        }
+
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab_host);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        BottomNavigationViewHelper.removeShiftMode(navigation);//disable BottomNavigationView shift mode
+    }
+
+}
