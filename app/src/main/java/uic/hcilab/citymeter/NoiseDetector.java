7678516432ab134324a70b67fd313d1a1457e4ee
Nina Sakhnini <sakhnini.nina@gmail.com>
2018-06-12 15:48:18 -0500

    Added noise detection class and a simple preview to home activity

---
+++ app/src/main/java/uic/hcilab/citymeter/NoiseDetector.java
@@ -0,0 +1,52 @@
+package uic.hcilab.citymeter;
+
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+import android.util.Log;
+
+public class NoiseDetector {
+
+    private AudioRecord mAudioRecord = null;
+    private int mBufferSize;
+
+    public void noiseDetect() {
+        mBufferSize = AudioRecord.getMinBufferSize(8000, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
+        mAudioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC, 8000, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, mBufferSize);
+        mAudioRecord.startRecording();
+        Log.i("RECORDER", "Started recording");
+        //double val = noiseLevel();
+       // Log.i("RECORDER", "dB = " + val);
+       // return val;
+    }
+
+    public void stop() {
+        if (mAudioRecord != null) {
+            mAudioRecord.stop();
+        }
+    }
+
+    public double noiseLevel() {
+        short[] buffer = new short[mBufferSize];//buffer to save the read sound pressure values
+        mAudioRecord.read(buffer, 0, mBufferSize);
+        int amplitude = 0;
+        for (short window : buffer)//Get the maximum amplitude in each window read (buffer size)
+        {
+            if (Math.abs(window) > amplitude)
+            {
+                amplitude = Math.abs(window);
+            }
+        }
+        double dB = 20 * Math.log10(amplitude);
+        //Calculating A-weighted dB
+        //Requires further calculations and transformations to get frequency
+        //TODO: Frequency
+        double f = 8000;//Frequency needs further calculations
+        double wa1 = 10* Math.log10((1.562339*(Math.pow(f,4)))/((Math.pow(f,2)+ Math.pow(107.65365,2))*(Math.pow(f,2)+ Math.pow(737.86223,2))));
+        double wa2 = 10* Math.log10((1.562339*(Math.pow(10,16)* Math.pow(f,4)))/(Math.pow(Math.pow(f,2)+ Math.pow(20.598997,2),2)* Math.pow(Math.pow(f, 2) + Math.pow(12194.22, 2),2)));
+        double wa = wa1 + wa2;
+        double dBA = wa + dB;
+        return dB;
+    }
+
+}
