37560ceb363bcc3d8b17021754feeb64e3126e74
yujaa <yujaaa0219@gmail.com>
2018-06-17 21:56:38 -0500

    Modify color-coded bar

--- app/src/main/java/uic/hcilab/citymeter/HomeActivity.java
+++ app/src/main/java/uic/hcilab/citymeter/HomeActivity.java
@@ -3,6 +3,7 @@ package uic.hcilab.citymeter;
 import android.Manifest;
 import android.app.Activity;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -11,6 +12,9 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -25,6 +29,7 @@ public class HomeActivity extends TabHost {
     //private NoiseDetector noiseDetector = new NoiseDetector();
     private ProgressBar noiseBar;
     private BluetoothController mBluetoothController;
+    private View view;
     //private SensorsDataHandler sensorsDataHandler;
     @Override
     public int getContentViewId() {
@@ -73,7 +78,52 @@ public class HomeActivity extends TabHost {
         //Create a bluetooth controller
         mBluetoothController= new BluetoothController(this, savedInstanceState);
         mBluetoothController.connectDevice();
+
+        //get View for change bar
+        final View view = findViewById(android.R.id.content);
+        view.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                // Removing layout listener to avoid multiple calls
+                if(Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+                    view.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                }
+                else {
+                    view.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                }
+                ///Bar
+                //PM2.5
+                float pm25_bar_loc;
+                int pm25_bar_width;
+                float pm25_range = 100f; //max - min //ToDo: Toy data
+                float pm25_value = 60f;              //ToDo: Toy data
+                ImageView pm25_bar = (ImageView) findViewById(R.id.pm25_bar);
+                ImageView pm25_thumb = (ImageView) findViewById(R.id.pm25_thumb);
+                TextView pm25_thumb_value = (TextView) findViewById(R.id.pm25_value);
+                pm25_bar_width = pm25_bar.getWidth();
+                pm25_bar_loc=pm25_bar.getX();
+                pm25_thumb.setX(pm25_bar_loc+ pm25_bar_width*(pm25_value/pm25_range)-(pm25_thumb.getWidth()/2));
+                pm25_thumb_value.setX(pm25_thumb.getX()+pm25_thumb.getWidth());
+                pm25_thumb_value.setText(pm25_value+"");
+
+                //PM2.5
+                float noise_bar_loc;
+                int noise_bar_width;
+                float noise_range = 100f; //max - min //ToDo: Toy data
+                float noise_value = 20f;              //ToDo: Toy data
+                ImageView noise_bar = (ImageView) findViewById(R.id.noise_bar);
+                ImageView noise_thumb = (ImageView) findViewById(R.id.noise_thumb);
+                TextView noise_thumb_value = (TextView) findViewById(R.id.noise_value);
+                noise_bar_width = noise_bar.getWidth();
+                noise_bar_loc=noise_bar.getX();
+                noise_thumb.setX(noise_bar_loc+ noise_bar_width*(noise_value/noise_range)-(noise_thumb.getWidth()/2));
+                noise_thumb_value.setX(noise_thumb.getX()+noise_thumb.getWidth());
+                noise_thumb_value.setText(noise_value+"");
+            }
+        });
     }
+
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
