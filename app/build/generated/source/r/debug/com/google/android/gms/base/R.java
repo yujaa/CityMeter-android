58a66d781c838c69bbdce45234a7b465faf0419a
Nina Sakhnini <sakhnini.nina@gmail.com>
2018-07-13 22:32:39 -0500

    merge with master

--- app/build/generated/source/r/debug/com/google/android/gms/base/R.java
+++ app/build/generated/source/r/debug/com/google/android/gms/base/R.java
@@ -52,15 +52,15 @@ public final class R {
         public static final int googleg_standard_color_18 = 0x7f070085;
     }
     public static final class id {
-        public static final int adjust_height = 0x7f080025;
-        public static final int adjust_width = 0x7f080026;
-        public static final int auto = 0x7f08002d;
-        public static final int dark = 0x7f080051;
-        public static final int icon_only = 0x7f08008e;
-        public static final int light = 0x7f08009b;
-        public static final int none = 0x7f0800be;
-        public static final int standard = 0x7f080112;
-        public static final int wide = 0x7f0801ba;
+        public static final int adjust_height = 0x7f080028;
+        public static final int adjust_width = 0x7f080029;
+        public static final int auto = 0x7f080031;
+        public static final int dark = 0x7f08005c;
+        public static final int icon_only = 0x7f0800a3;
+        public static final int light = 0x7f0800b3;
+        public static final int none = 0x7f0800d8;
+        public static final int standard = 0x7f08012e;
+        public static final int wide = 0x7f0801d7;
     }
     public static final class string {
         public static final int common_google_play_services_enable_button = 0x7f0e002a;
